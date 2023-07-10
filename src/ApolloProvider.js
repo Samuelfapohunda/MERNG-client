95a019b1c03a058b99c4ca3dd698236a69057d99
Samuel Fapohunda <87587218+Samuelfapohunda@users.noreply.github.com>
2023-07-10 23:51:34 +0100

    Update ApolloProvider.js

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -8,7 +8,7 @@ import {setContext} from 'apollo-link-context'
 import {createUploadLink} from 'apollo-upload-client';
 
 const httpLink = createHttpLink({
-    uri: 'http://localhost:5000'
+    uri: 'https://merng.onrender.com/'
 })
 
 const uploadLink = createUploadLink({
